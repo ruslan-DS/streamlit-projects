1dfbfa9cbde2b9e03bf91418abd8e48431d2d521
ruslan-DS <ruslanvolo9@gmail.com>
2023-10-18 17:31:13 +0300

    Full made two projects

--- AAPLE/strim-aaple.py
+++ AAPLE/strim-aaple.py
@@ -6,7 +6,7 @@ st.set_page_config(layout='wide')
 
 st.write(
     """
-# Визуализация котеровок компании **APPLE** с начала выхода компании на **IPO**
+# Визуализация котеровок компании **Apple** с начала выхода компании на **IPO**
     """
 )
 
@@ -15,3 +15,19 @@ tick = yf.Ticker('AAPL')
 df_history = tick.history(period='1m', start='1980-12-12')
 
 st.line_chart(df_history['Close'])
+
+# Зависимость между котировками Google и Apple
+
+st.write("""
+# Корреляция между котировками акций ***Google*** и ***Apple***
+"""
+)
+
+new_df = pd.DataFrame({
+    "GOOGLE": yf.Ticker('GOOGL').history(period='1m', start='1980-12-12')['Close'],
+    'APPLE': df_history['Close']
+})
+
+st.scatter_chart(data=new_df, x='GOOGLE', y='APPLE')
+st.info('Как видим из графика, есть достаточная четкая корреляция, \
+которая сопровождается изменением котировок Apple на аналогичные изменения котировок Google.')
